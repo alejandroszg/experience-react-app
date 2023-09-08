4efe6f1ee78173d0145c0546b0b6c6d7ed5c55d0
Alejandro Sanchez <alejandroszg@gmail.com>
2023-09-08 15:35:01 -0300

    finishing project

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -3,10 +3,18 @@ import React from "react";
 const Offers = () => {
   return (
     <div className="max-w-[900px] gap-x-8 m-auto px-4 py-12 flex flex-wrap justify-between">
-      <p className="text-lg font-bold text-gray-700">Booking Incentives</p>
-      <p className="text-lg font-bold text-gray-700">Competitive Rates</p>
-      <p className="text-lg font-bold text-gray-700">Book With Flexibility</p>
-      <p className="text-lg font-bold text-gray-700">Trip Insurance</p>
+      <p className="text-lg font-bold text-gray-700 cursor-pointer">
+        Booking Incentives
+      </p>
+      <p className="text-lg font-bold text-gray-700 cursor-pointer">
+        Competitive Rates
+      </p>
+      <p className="text-lg font-bold text-gray-700 cursor-pointer">
+        Book With Flexibility
+      </p>
+      <p className="text-lg font-bold text-gray-700 cursor-pointer">
+        Trip Insurance
+      </p>
     </div>
   );
 };
